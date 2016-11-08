718528d5903e0d05d5373a46fdbab044dd6260c5
olim7t <omichallat+github@gmail.com>
2016-11-08 09:31:48 -0800

    Revert "JAVA-1263: Move Compressors into own files, separate direct & heap code"
    This reverts commit 2890c04a5ba9d7abbbc74b5e50555d86fce848ae.

--- driver-core/src/main/java/com/datastax/driver/core/ProtocolOptions.java
+++ driver-core/src/main/java/com/datastax/driver/core/ProtocolOptions.java
@@ -41,7 +41,7 @@ public class ProtocolOptions {
         SNAPPY("snappy") {
             @Override
             FrameCompressor compressor() {
-                return SnappyCompressor.instance;
+                return FrameCompressor.SnappyCompressor.instance;
             }
         },
         /**
@@ -50,7 +50,7 @@ public class ProtocolOptions {
         LZ4("lz4") {
             @Override
             FrameCompressor compressor() {
-                return LZ4Compressor.instance;
+                return FrameCompressor.LZ4Compressor.instance;
             }
         };
 
